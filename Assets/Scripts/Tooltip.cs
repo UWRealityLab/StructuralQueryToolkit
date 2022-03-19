0e5e315ab893ed57f4f58d764fd6cb8c5c51d82e
Jacky Mooc <jackymooc@gmail.com>
2022-03-19 10:03:31 -0700

    Fixed pause menu not working. Fixed buttons tooltip not showing. Fixed stereonet fullscreen legend.

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -8,16 +8,13 @@ using UnityEngine.EventSystems;
 
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    
-    
-    
     [TextArea]
     public string HeaderText;
     [TextArea(4, 32)]
     public string DescriptionText;
     public Vector2 Offset;
 
-    private Coroutine _ShowTooltipCo;
+    private Coroutine _showTooltipCo;
     private RectTransform _rectTransform;
 
     private void Start()
@@ -27,29 +24,29 @@ public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        _ShowTooltipCo = StartCoroutine(ShowTooltipCoroutine());
+        _showTooltipCo = StartCoroutine(ShowTooltipCoroutine());
     }
 
     private IEnumerator ShowTooltipCoroutine()
     {
         var elapsedTime = 0f;
-        while (elapsedTime < TooltipSystem.instance.HoverTime)
+        while (elapsedTime < TooltipSystem.Instance.HoverTime)
         {
             elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
 
-        _ShowTooltipCo = null;
-        TooltipSystem.Show(new Vector2(transform.position.x, transform.position.y), Offset, HeaderText, DescriptionText);
+        _showTooltipCo = null;
+        TooltipSystem.Instance.Show(new Vector2(transform.position.x, transform.position.y), Offset, HeaderText, DescriptionText);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (_ShowTooltipCo != null)
+        if (_showTooltipCo != null)
         {
-            StopCoroutine(_ShowTooltipCo);
-            _ShowTooltipCo = null;
+            StopCoroutine(_showTooltipCo);
+            _showTooltipCo = null;
         }
-        TooltipSystem.Hide();
+        TooltipSystem.Instance.Hide();
     }
 }
